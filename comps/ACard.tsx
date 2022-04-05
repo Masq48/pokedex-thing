7f7de43917c0b827fb17a4f8e73b627bfb283fff
Scott Christensen <Masqchr@gmail.com>
2022-04-04 18:47:05 -0700

    Changed Page Name

--- comps/ACard.tsx
+++ comps/ACard.tsx
@@ -2,7 +2,7 @@ import { NextFunctionComponent } from "next"
 
 const ACard: NextFunctionComponent = ( {name, abilities} ) => {
   return (
-    <div className="w-1/4 bg-gray-700 rounded-lg">
+    <div className="w-1/4 bg-gray-700 rounded-lg m-1 p-4">
       <h1 className="text-4xl text-center">{name}</h1>
       <ul>
         {abilities.map(ability =>
