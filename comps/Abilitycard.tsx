b2e8be2d0acf188e95d488b495ecc4ac2027bb06
Scott Christensen <Masqchr@gmail.com>
2022-04-08 13:31:39 -0700

    Added Cap Utility

--- comps/Abilitycard.tsx
+++ comps/Abilitycard.tsx
@@ -1,18 +1,22 @@
 import { NextFunctionComponent } from "next"
 
-const Abilitycard: NextFunctionComponent = ({ abilities }) => {
+import cap from "../public/utils"
 
-  const cap = (str: string) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+type props = {
+  abilities: object
+}
+
+const Abilitycard: NextFunctionComponent<props> = ({ abilities }) => {
 
   return (
-    <div className="w-1/4 bg-gray-700 rounded-lg m-1 p-4">
+    <div className="w-1/3 bg-gray-700 rounded-lg m-1 p-4">
       <h1 className="text-4xl text-center">Abilities</h1>
       <ul className="flex my-5">
-        {abilities.map(ability =>
-          (<li className="flex-1 text-center">{ability.is_hidden ?  <abbr title="Hidden">{cap(ability.ability.name)}</abbr> : cap(ability.ability.name)}</li>)
-        )}
+
+        {abilities.map(ability =>(
+          <li className="flex-1 text-center">{ability.is_hidden ?  <abbr title="Hidden">{cap(ability.ability.name)}</abbr> : cap(ability.ability.name)}</li>
+        ))}
+
       </ul>
     </div>
   )
---
+++ public/utils.ts
@@ -0,0 +1,5 @@
+const cap = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export default cap
