ca0f6fcc504847596773d3e54aa2144e36da21be
Masq48 <99148513+Masq48@users.noreply.github.com>
2022-04-08 19:44:06 -0700

    Added Abilities To Navbar

--- comps/Navbar.tsx
+++ comps/Navbar.tsx
@@ -15,6 +15,11 @@ const Navbar: NextFunctionComponent = () => {
               Mon
             </a>
           </Link>
+          <Link href="/">
+            <a>
+              Abilities
+            </a>
+          </Link>
         </div>
       </div>
     </nav>
