c00c092998755b697c217f28e5f65b88edbf958d
Masq48 <99148513+Masq48@users.noreply.github.com>
2022-10-18 13:17:07 -0700

    Thing -> Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next"
 
 const Custom404: NextPage = () => {
   return(
-    <h1 className="font-bold text-5xl text-white text-center">The Thing You Requested Does Not Exist</h1>
+    <h1 className="font-bold text-5xl text-white text-center">The Page You Requested Does Not Exist</h1>
   )
 }
 
